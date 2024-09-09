08fd7380fb638327707612116d1765b8a2753280
kimseongjae <stwin755@gmail.com>
2024-09-09 18:02:03 +0900

    Test2

--- src/java/MiniProject.java
+++ src/java/MiniProject.java
@@ -7,6 +7,8 @@ public class MiniProject {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("화씨온도를 입력하시오: ");
+		int cTemp = 0;
+		System.out.println("섭씨온도는 " + cTemp);
 	}
 
 }
