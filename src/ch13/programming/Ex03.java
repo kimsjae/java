57aa01eed0e9fe4efad0787cdb9e535a25ad131d
kimseongjae <stwin755@gmail.com>
2024-10-10 19:26:35 +0900

    Add Programming Exercise 03 in Ch13

---
+++ src/ch13/programming/Ex03.java
@@ -0,0 +1,18 @@
+package ch13.programming;
+
+import java.util.ArrayList;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		list.add("e");
+		
+		System.out.println(list);
+	}
+
+}
